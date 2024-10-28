2be02c099fd215f03bdfa9a4d335c312ea3a280e
Serg Budnikov <sergei.budnikov@smartseeds.ru>
2024-10-28 23:08:17 +0300

    save client info

--- app.rb
+++ app.rb
@@ -26,20 +26,23 @@ end
 
 post '/visit' do
 
-	@username = params[:username]
-	@phone = params[:phone]
-	@datetime = params[:datetime]
-	@barber = params[:barber]
-	@color = params[:color]
-
-	# name, phone, datestamp, barber, color
-	c = Client.new
-	c.name = @usernamec
-	c.phone = @phone
-	c.datestamp = @datetime
-	c.barber = @barber
-	c.color = @color
+	c = Client.new params[:client]
 	c.save
+	
+	# @username = params[:username]
+	# @phone = params[:phone]
+	# @datetime = params[:datetime]
+	# @barber = params[:barber]
+	# @color = params[:color]
+
+	# # name, phone, datestamp, barber, color
+	# c = Client.new
+	# c.name = @usernamec
+	# c.phone = @phone
+	# c.datestamp = @datetime
+	# c.barber = @barber
+	# c.color = @color
+	# c.save
 
 	erb "<h2>Спасибо, вы записались!</h2>"
 end
