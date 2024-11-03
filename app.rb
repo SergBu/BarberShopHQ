f48542fa1594287eda8d5d4ea3ab342553424e9c
Serg Budnikov <sergei.budnikov@smartseeds.ru>
2024-11-04 01:06:57 +0300

    add barber and client view

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/activerecord'
 set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 class Client < ActiveRecord::Base # это модель, а не сущность
-	validates :name, presence: true # это хэш 
+	validates :name, presence: true, length: { minimum: 3 } # это хэш 
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true
@@ -17,11 +17,11 @@ class Barber < ActiveRecord::Base
 end
 
 before do
-	@barbers = Barber.order "created_at DESC"
+	@barbers = Barber.order "created_at DESC" #Barber.all
 end
 
 get '/' do
-	erb :index #вид
+	erb :index #вид или представление
 end
 
 get '/visit' do
@@ -32,6 +32,7 @@ end
 post '/visit' do
 
 	@c = Client.new params[:client]
+	#@c = Client.new 'name' => 'Jhon', 'phone' => '12345', 'datestamp' => '2014/09/23 22:00', 'color' => '#7db148'
 	if @c.save
 		erb "<h2>Спасибо, вы записались!</h2>"
 	else
@@ -54,5 +55,25 @@ post '/visit' do
 	# c.color = @color
 	# c.save
 
+end
+
+get '/visit' do
+	@c = Client.new
+	erb :visit
+end
+
+get '/barber/:id' do
+	@barber = Barber.find(params[:id])
+	erb :barber
+end
+
+get '/bookings' do
+	@clients = Client.order('created_at DESC')
+	erb :bookings
+end
+
+get '/client/:id' do
+	@client = Client.find(params[:id])
+	erb :client
 end
 
